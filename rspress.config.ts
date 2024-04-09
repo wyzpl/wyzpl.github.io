d6223c953dfb850db2078c574ccfa88549bacb79
wyz <wangyuzhou@richerlink.com>
2024-04-09 16:09:23 +0800

    first build

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "rspress/config";
 export default defineConfig({
   root: path.join(__dirname, "docs"),
   title: "开发个锤子",
+  base: "/blog/",
   description: "Rspack-based Static Site Generator",
   icon: "/rspress-icon.png",
   logo: {
