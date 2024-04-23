b4ddb9ef92383e55e97830e59fc976234b7fc82e
wyz <wangyuzhou@richerlink.com>
2024-04-23 17:41:02 +0800

    update doc

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,27 +1,23 @@
 import * as path from "path";
 import { defineConfig } from "rspress/config";
+import alignImage from "rspress-plugin-align-image";
 
 export default defineConfig({
   root: path.join(__dirname, "docs"),
   title: "JSZhiShu",
   base: "/blog/",
   description: "朝如青丝暮成雪 人生得意须尽欢",
-  logoText:'JSZhiShu',
+  logoText: "JSZhiShu",
   // icon: "/rspress-icon.png",
   // logo: {
   //   light: "/rspress-light-logo.png",
   //   dark: "/rspress-dark-logo.png",
   // },
-  route:{
-    cleanUrls:true
+  plugins: [alignImage()],
+  route: {
+    cleanUrls: true,
   },
   themeConfig: {
-    socialLinks: [
-      {
-        icon: "github",
-        mode: "link",
-        content: "https://github.com/web-infra-dev/rspress",
-      },
-    ],
+    lastUpdated: true,
   },
 });
